5be832f8998a8f80d4779d2bbd0625ef6c50eaa3
nikicahecimovic <116574607+nikicahecimovic@users.noreply.github.com>
2024-07-16 15:15:36 +0200

    add desctiption and co2

--- scraper.js
+++ scraper.js
@@ -1,9 +1,11 @@
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 
+// Add stealth plugin to puppeteer
 puppeteer.use(StealthPlugin());
 
 (async () => {
+  // Get the URL from the command line arguments
   const url = process.argv[2];
   if (!url) {
     console.error('Please provide a URL as the first argument');
@@ -13,16 +15,20 @@ puppeteer.use(StealthPlugin());
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
+  // Set user-agent and other necessary headers
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
   await page.setExtraHTTPHeaders({
     'Accept-Language': 'en-US,en;q=0.9'
   });
 
   try {
+    // Open the URL
     await page.goto(url, { waitUntil: 'networkidle2' });
 
+    // Wait for necessary elements to be available
     await page.waitForSelector('#root > div > div.cG3B1 > div.XLH5_ > aside > article > section > div > div.GOIOV.fqe3L.EevEz', { timeout: 60000 });
 
+    // Extract car details using the provided CSS selectors
     const details = await page.evaluate(() => {
       const getTextContent = (selector) => {
         const element = document.querySelector(selector);
@@ -37,10 +43,13 @@ puppeteer.use(StealthPlugin());
         power: getTextContent('#root > div > div.cG3B1 > div.XLH5_ > div > article:nth-child(2) > section > div > div > div:nth-child(2) > div > div.geJSa'),
         fuelType: getTextContent('#root > div > div.cG3B1 > div.XLH5_ > div > article:nth-child(2) > section > div > div > div:nth-child(3) > div > div.geJSa'),
         transmission: getTextContent('#root > div > div.cG3B1 > div.XLH5_ > div > article:nth-child(2) > section > div > div > div:nth-child(4) > div > div.geJSa'),
-        dateOfManufacturing: getTextContent('#root > div > div.cG3B1 > div.XLH5_ > div > article:nth-child(2) > section > div > div > div:nth-child(5) > div > div.geJSa')
+        dateOfManufacturing: getTextContent('#root > div > div.cG3B1 > div.XLH5_ > div > article:nth-child(2) > section > div > div > div:nth-child(5) > div > div.geJSa'),
+        co2: getTextContent('#root > div > div.cG3B1 > div.XLH5_ > div > article:nth-child(3) > section > div > div > dl > dd:nth-child(20)'),
+        description: getTextContent('#root > div > div.cG3B1 > div.XLH5_ > div > article:nth-child(6) > section > div > div > div'),
       };
     });
 
+    // Print the details
     console.log(details);
 
   } catch (error) {
